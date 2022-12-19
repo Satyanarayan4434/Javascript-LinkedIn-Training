30caedc00d06f48dc9859ac9df0fd57b69252e82
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2022-12-19 10:51:36 +0530

    Update script.js

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,12 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const car ={
+    celerio:500000,
+    scorpio:900000,
+    BMW:3000000,
+    MG:{
+      MG_Hector:2500000,
+      MG_X:5000000
+    }
+}
